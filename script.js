b246e5983c6e01b0f0e218aa1d6ac52d7182009f
tanish699 <tanishwadhwa2310@gmail.com>
2024-12-09 14:56:52 +0530

    updating script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const imgContainer = document.querySelector(".showcase > div");
+const img = document.querySelector(".showcase img");
+const shadow = document.querySelector(".shadow");
+
+const thumb = document.querySelector(".thumb img");
+const titleOverlay = document.querySelector(".titleOverlay");
+const title = document.querySelector(".titleText");
+const desc = document.querySelector(".description");
+
+const sizes = document.querySelector(".sizes > li");
+const stars = document.querySelector(".stars span");
+const price = document.querySelector(".price");
+const colorBtn = document.querySelector(".color");
+
+const pag = document.querySelector(".pag");
+const prev = document.querySelector(".arr-left");
+const next = document.querySelector(".arr-right");
+const shoeNum = document.querySelector(".shoe-num");
+const shoeTotal = document.querySelector(".shoe-total");
+
+let id= 1;
+let colorType = 1;
+let shoe = 1;
+
